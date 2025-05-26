d4867328d1fa0acafa75ed46c06ffd8883617140
Mahmud-Alam <official.mahmud23@gmail.com>
2025-05-27 00:47:17 +0600

    add cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import  router  from "./routes/routes.js";
+import cors from 'cors'
 
 const PORT = 8000;
 
@@ -7,5 +8,6 @@ const app = express();
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
 
+app.use(cors());
 app.use("/api", router);
 app.use((req, res)=>res.status(404).json({message:"Endpoint is not found!"}));
