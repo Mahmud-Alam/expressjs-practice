1c6128d42ff683075f3e3244d32aab4bdebc18c7
Mahmud-Alam <official.mahmud23@gmail.com>
2025-05-26 22:58:09 +0600

    path params filter option with allowed paths only

--- server.js
+++ server.js
@@ -1,36 +1,43 @@
-import express from 'express'
-import { startups } from './data/data.js';
+import express from "express";
+import { startups } from "./data/data.js";
 
 const PORT = 8000;
 
 const app = express();
 
-app.listen(PORT, ()=>console.log(`Server is running on PORT ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
 
-app.get('/api',(req, res)=>{
-    let filterData = startups;
-    
-    if(req.query){
-        Object.entries(req.query).forEach(([key,value])=>{
-            filterData = filterData.filter(data=>{
-                if (!(key in data)) return false;
-                return String(data[key]).toLowerCase() === String(value).toLowerCase();
-            })
-        })
-    }
-    
-    res.statusCode = 200
-    res.json(filterData)
-})
+app.get("/api", (req, res) => {
+  let filterData = startups;
 
-app.get('/api/:key/:value', (req, res)=>{
-    let filterData = startups;
+  if (req.query) {
+    Object.entries(req.query).forEach(([key, value]) => {
+      filterData = filterData.filter((data) => {
+        if (!(key in data)) return false;
+        return String(data[key]).toLowerCase() === String(value).toLowerCase();
+      });
+    });
+  }
 
-    filterData = filterData.filter(data=>{
-        return String(data[req.params.key]).toLowerCase() === String(req.params.value).toLowerCase();
-    })
-    
-    res.json(filterData)
-    
-})
+  res.status(200).json(filterData);
+});
 
+app.get("/api/:key/:value", (req, res) => {
+  let filterData = startups;
+  const { key, value } = req.params;
+  const allowedFields = ["country", "continent", "industry", "has_mvp"];
+  const message = {
+    code: "404",
+    message:
+      "Search field not allowed. Please use only 'country', 'continent', 'industry', 'has_mvp'",
+  };
+
+  if (allowedFields.includes(key)) {
+    filterData = filterData.filter((data) => {
+      return String(data[key]).toLowerCase() === String(value).toLowerCase();
+    });
+    res.status(200).json(filterData);
+  } else {
+    res.status(400).json(message);
+  }
+});
