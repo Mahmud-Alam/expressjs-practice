1e806bde0a82c6d2f4ec59d7fbf0995bbfcd186f
Mahmud-Alam <official.mahmud23@gmail.com>
2025-05-26 20:16:50 +0600

    create query params filter option

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import { startups } from './data/data.js';
+
+const PORT = 8000;
+
+const app = express();
+
+app.listen(PORT, ()=>console.log(`Server is running on PORT ${PORT}`));
+
+app.get('/api',(req, res)=>{
+    let filterData = startups;
+    
+    if(req.query){
+        Object.entries(req.query).forEach(([key,value])=>{
+            filterData = filterData.filter(data=>{
+                if (!(key in data)) return false;
+                return String(data[key]).toLowerCase() === String(value).toLowerCase();
+            })
+        })
+    }
+    
+    res.statusCode = 200
+    res.json(filterData)
+})
+
